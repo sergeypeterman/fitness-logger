fda3e168fa2211008a00016764afa45916d0fbfd
sergeypeterman <112394347+sergeypeterman@users.noreply.github.com>
2023-04-19 18:26:05 +0100

    minor

--- src/pages/index.js
+++ src/pages/index.js
@@ -136,13 +136,13 @@ export default function Home() {
   };
 
   return (
-    <main className="h-screen relative overflow-auto flex flex-col m-auto w-full ">
+    <main className="h-screen relative overflow-auto flex flex-col m-auto w-full box-border">
       <div
         name="widget"
         className="max-w-lg min-w-sm shadow-xl flex flex-col p-5 m-auto border-slate-400 border-2 rounded-2xl border-solid"
       >
         <div name="logo" className="font-header text-5xl pb-5 m-auto ">
-          FITNESS LOGGER {fetched}
+          FITNESS LOGGER 
         </div>
         {selectedProgram && (
           <Settings
