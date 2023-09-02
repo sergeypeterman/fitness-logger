cbd2ca1cc1eb48c74175554727c988fc7654b325
sergeypeterman <112394347+sergeypeterman@users.noreply.github.com>
2023-09-02 09:01:23 +0100

    meta and title

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,6 +3,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+      <title>Fitness Logger app</title>
+      <meta name='description' content='Fitness Logger helps to track workouts'/>
       <Head />
       <body>
         <Main />
