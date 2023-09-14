d8b5f7c00a80d301f1caece0289c792057e01841
sergeypeterman <112394347+sergeypeterman@users.noreply.github.com>
2023-09-14 11:58:54 +0100

    minor updates

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,10 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>Fitness Logger app</title>
         <meta
           name="description"
-          content="Fitness Logger app helps to track workouts"
+          content="Fitness Logger helps to track workouts"
         />
       </Head>
       <body>
